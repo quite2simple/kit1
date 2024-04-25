8478ef36b5af30bb74d01975b12ddee1ad09936a
quite2simple <simplechuck882@gmail.com>
2024-04-26 00:12:19 +0300

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,38 @@
+<main>
+  <h1>Heading 1</h1>
+  <h2>Heading 2</h2>
+  <h3>Heading 3</h3>
+
+  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero mollitia quia, cum, earum ipsam eum recusandae possimus nam aspernatur vitae sint eveniet quo itaque omnis eligendi a placeat excepturi perferendis.
+  Voluptatem aliquid qui quae. Ratione laboriosam iusto cum ex saepe libero, ut, qui molestiae reiciendis in possimus dolorum consequatur quam esse iste modi rem? Earum, debitis expedita. Perferendis, labore aliquam.</p>
+
+  <span class="text-small">Small text</span>
+
+  <div class="card">
+    <h2>Card</h2>
+    <span class="text-small">Small text</span>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, placeat. Officia perspiciatis quae error recusandae, quos voluptates quam. Expedita fugiat vitae, incidunt necessitatibus nisi exercitationem sed sit harum nemo ipsum.</p>
+    <button class="btn primary">Primary button</button>
+    <button class="btn secondary">Secondary button</button>
+  </div>
+
+  <button class="btn accent">Accent button</button>
+  <button class="btn disabled">Disabled</button>
+  <button class="btn success">Success</button>
+  <button class="btn warning">Warning</button>
+  <button class="btn error">Error</button>
+
+  <div class="message">
+    <p>Message</p>
+  </div>
+  <div class="message success">
+    <p>Success</p>
+  </div>
+  <div class="message warning">
+    <p>Warning</p>
+  </div>
+  <div class="message error">
+    <p>Error</p>
+  </div>
+
+</main>
